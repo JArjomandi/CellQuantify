524570506de63e6d0589c73b3d4c2f0c871caf77
J.Arjo <arjomandi.jasmin@gmail.com>
2024-11-21 18:52:09 +0100

    red intensity range filtering for aggregates, min intensity and diameter filtering, red channel check

--- Cell_aggregate_quantifier.py
+++ Cell_aggregate_quantifier.py
@@ -22,8 +22,8 @@ def analyze_image(image_path):
 
     # Enhanced detection for red aggregates
     red_threshold = filters.threshold_otsu(red_channel) * 1.2
-    red_aggregates_mask = red_channel > red_threshold
-    red_aggregates_mask = morphology.remove_small_objects(red_aggregates_mask, min_size=5)
+    red_aggregates_mask = (red_channel > red_threshold) & (red_channel > img[:, :, 1]) & (red_channel > img[:, :, 0])
+    red_aggregates_mask = morphology.remove_small_objects(red_aggregates_mask, min_size=10)
 
     # Distance transform for watershed segmentation
     distance = ndi.distance_transform_edt(red_aggregates_mask)
@@ -33,6 +33,16 @@ def analyze_image(image_path):
 
     # Measure properties of red aggregates after watershed
     red_aggregates_props = measure.regionprops(labels, intensity_image=red_channel)
+
+    # Apply additional filtering based on size and intensity
+    min_intensity = 150  # Minimum intensity for a region to be considered
+    min_diameter = 2.0  # Minimum equivalent diameter for a valid aggregate
+    red_aggregates_props = [
+        prop for prop in red_aggregates_props
+        if prop.mean_intensity > min_intensity and prop.equivalent_diameter > min_diameter
+    ]
+
+    # Count remaining red aggregates and measure diameters
     red_aggregates_diameters = [prop.equivalent_diameter for prop in red_aggregates_props]
     num_red_aggregates = len(red_aggregates_diameters)
 
